e1281261a3ef1b18055767f9ad88a3caaf3f3dd1
wyulliam <william.da.silva@verizonconnect.com>
2019-11-26 09:13:18 +0000

    ScreenShooter

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ScreenShooter
+{
+    class Program
+    {
+        private static DirectoryInfo dir = new DirectoryInfo("C:\\temp");
+        static async Task Main(string[] args)
+        {
+            if (!dir.Exists) dir.Create();
+
+            while (true)
+            {
+                await StartPrinting();
+                Thread.Sleep(3000);
+            }
+
+        }
+
+        private async static Task StartPrinting()
+        {
+            var name = DateTime.Now.ToString("MMddmmss");
+            PrintScreen ps = new PrintScreen();
+            ps.CaptureScreenToFile($"{dir}\\{name}.png", ImageFormat.Png);
+        }
+    }
+}
